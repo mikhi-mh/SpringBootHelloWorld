93de93ba3d0d120b1556fcb09bd27a0c7ee799fe
mikhi-mh <hmikhilesh@gmail.com>
2023-06-08 18:01:31 +0530

    method added

--- src/main/java/com/example/springboothelloworld/MyController.java
+++ src/main/java/com/example/springboothelloworld/MyController.java
@@ -1,10 +1,17 @@
 package com.example.springboothelloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/msg")
 public class MyController {
-    
+
+    @GetMapping("getMsg/{name}")
+    public String getMsg(@PathVariable String name){
+        return "नमस्कार " + name + " how are you";
+    }
+
 }
